c7ab150a8c25852017eed067ade0b64cbf7dc774
Emmanuel Egomson <fegomson@gmail.com>
2021-04-01 16:29:06 -0500

    Implemented the movie controls

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import MovieControls from "./MovieControls"
 
 const MovieCard = ({ movie, type }) => {
   const {title, poster_path} = movie
@@ -7,6 +8,7 @@ const MovieCard = ({ movie, type }) => {
       <div className="overlay"></div>
 
       {poster_path ? (<img src={`https://image.tmdb.org/t/p/w300${poster_path}`} alt={`${title} Poster`}/>) : (<div className="filler-poster"></div>)}
+      <MovieControls type={type} movie={movie}/>
     </div>
   )
 }

8428c9524bb282ed6fe666dc9821656a1b421d36
Emmanuel Egomson <fegomson@gmail.com>
2021-04-01 12:56:37 -0500

    Setting and getting the search query with useState

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Add = () => {
+  const [query, setQuery] = useState('')
+
+  const handleChangeQuery = (e) => {
+    e.preventDefault()
+    setQuery(e.target.value)
+  }
+
   return (
     <div className="add-page">
       <div className="container">
         <div className="add-content">
           <div className="input-wrapper">
-            <input type="text" placeholder="Search for a movie"/>
+            <input type="text" value={query} onChange={handleChangeQuery} placeholder="Search for a movie" />
           </div>
         </div>
       </div>

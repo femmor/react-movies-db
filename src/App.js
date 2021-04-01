0b5530158f3799de6b7d53aa2d60f0af236de6c4
Emmanuel Egomson <fegomson@gmail.com>
2021-04-01 12:46:40 -0500

    Created the app components with the Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import './App.css';
+import Header from './components/Header';
+import Watchlist from './components/Watchlist';
+import Watched from './components/Watched';
+import Add from './components/Add';
+import "./lib/font-awesome/css/all.min.css"
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Watchlist}/>
+        <Route path="/watched" component={Watched}/>
+        <Route path="/add" component={Add}/>
+      </Switch>
+    </Router>
   );
 }
 

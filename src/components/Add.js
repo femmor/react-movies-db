0f606365680f91e81f38f8baddaa317432f322c8
Emmanuel Egomson <fegomson@gmail.com>
2021-04-01 13:22:54 -0500

    Query the TMDB API and get SearchQuery

--- src/components/Add.js
+++ src/components/Add.js
@@ -5,7 +5,14 @@ const Add = () => {
 
   const handleChangeQuery = (e) => {
     e.preventDefault()
-    setQuery(e.target.value)
+    const searchQuery = e.target.value
+    const api_key = process.env.REACT_APP_TMDB_KEY
+    setQuery(searchQuery)
+
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&page=1&include_adult=false&query=${searchQuery}`)
+    .then(res => res.json())
+    .then(data => console.log(data))
   }
 
   return (
